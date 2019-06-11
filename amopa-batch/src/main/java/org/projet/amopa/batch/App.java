7144fda75796df41b70304d3a11132bdb5c4d3e5
lopotono <30410435+lopotono@users.noreply.github.com>
2019-06-11 18:29:20 +0200

    Add amopa-batch

---
+++ amopa-batch/src/main/java/org/projet/amopa/batch/App.java
@@ -0,0 +1,13 @@
+package org.projet.amopa.batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
