8a6617b00cdb95ed9a56814f44daf3cf59d4d39e
lopotono <30410435+lopotono@users.noreply.github.com>
2019-06-27 16:33:36 +0200

    Add ListActivityExpoAction.java

---
+++ amopa-webapp/src/main/java/org/projet/amopa/webapp/action/ListActivityExpoAction.java
@@ -0,0 +1,41 @@
+package org.projet.amopa.webapp.action;
+
+import java.util.List;
+
+import org.projet.amopa.model.Activity;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ListActivityExpoAction extends AbstractAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -916600193274037661L;
+	
+	private List<Activity> listActivity;
+	private String genre;
+
+	public List<Activity> getListActivity() {
+		return listActivity;
+	}
+
+	public void setListActivity(List<Activity> listActivity) {
+		this.listActivity = listActivity;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	public String execute() {
+		
+		// Appel de la méthode getListActivityByExpo
+		listActivity = getManagerFactory().getActivityManager().getListActivityByExpo(genre);
+		return ActionSupport.SUCCESS;		
+	}
+}
