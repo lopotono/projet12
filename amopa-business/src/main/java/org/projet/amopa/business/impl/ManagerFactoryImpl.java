db77a2b36cd1ec0e7ba890766b29a874d250195c
lopotono <30410435+lopotono@users.noreply.github.com>
2019-06-27 16:22:33 +0200

    Update ManagerFactoryImpl.java

--- amopa-business/src/main/java/org/projet/amopa/business/impl/ManagerFactoryImpl.java
+++ amopa-business/src/main/java/org/projet/amopa/business/impl/ManagerFactoryImpl.java
@@ -1,11 +1,15 @@
 package org.projet.amopa.business.impl;
 
+import org.projet.amopa.business.contract.ActivityManager;
 import org.projet.amopa.business.contract.ManagerFactory;
+import org.projet.amopa.business.contract.ReservationManager;
 import org.projet.amopa.business.contract.UserManager;
 
 public class ManagerFactoryImpl implements ManagerFactory  {
 	
 	private UserManager userManager;
+	private ActivityManager activityManager;
+	private ReservationManager reservationManager;
 
 	public UserManager getUserManager() {
 		return userManager;
@@ -14,4 +18,20 @@ public class ManagerFactoryImpl implements ManagerFactory  {
 	public void setUserManager(UserManager userManager) {
 		this.userManager = userManager;
 	}
+
+	public ActivityManager getActivityManager() {
+		return activityManager;
+	}
+
+	public void setActivityManager(ActivityManager activityManager) {
+		this.activityManager = activityManager;
+	}
+
+	public ReservationManager getReservationManager() {
+		return reservationManager;
+	}
+
+	public void setReservationManager(ReservationManager reservationManager) {
+		this.reservationManager = reservationManager;
+	}	
 }
