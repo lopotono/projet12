45918ce5cf5163e466073bc414bef486ffb83ce3
lopotono <30410435+lopotono@users.noreply.github.com>
2019-06-27 16:21:11 +0200

    Update AbstractManager.java

--- amopa-business/src/main/java/org/projet/amopa/business/impl/AbstractManager.java
+++ amopa-business/src/main/java/org/projet/amopa/business/impl/AbstractManager.java
@@ -1,6 +1,7 @@
 package org.projet.amopa.business.impl;
 
 import org.projet.amopa.consumer.contract.DaoFactory;
+import org.springframework.transaction.PlatformTransactionManager;
 
 public abstract class AbstractManager {
 	
@@ -13,4 +14,14 @@ public abstract class AbstractManager {
 	public void setDaoFactory(DaoFactory daoFactory) {
 		this.daoFactory = daoFactory;
 	}
+	
+	private PlatformTransactionManager platformTransactionManager;
+	
+	public PlatformTransactionManager getPlatformTransactionManager() {
+		return platformTransactionManager;
+	}
+
+	public void setPlatformTransactionManager(PlatformTransactionManager platformTransactionManager) {
+		this.platformTransactionManager = platformTransactionManager;
+	}		
 }
