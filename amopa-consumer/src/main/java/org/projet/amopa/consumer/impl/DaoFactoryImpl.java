b7c3c00ce77d5cd223a6eb8f6dea5ee0ecc117a0
lopotono <30410435+lopotono@users.noreply.github.com>
2019-06-27 16:16:46 +0200

    Update DaoFactoryImpl.java

--- amopa-consumer/src/main/java/org/projet/amopa/consumer/impl/DaoFactoryImpl.java
+++ amopa-consumer/src/main/java/org/projet/amopa/consumer/impl/DaoFactoryImpl.java
@@ -1,11 +1,15 @@
 package org.projet.amopa.consumer.impl;
 
+import org.projet.amopa.consumer.contract.ActivityDAO;
 import org.projet.amopa.consumer.contract.DaoFactory;
+import org.projet.amopa.consumer.contract.ReservationDAO;
 import org.projet.amopa.consumer.contract.UserDAO;
 
 public class DaoFactoryImpl implements DaoFactory {
 
 	private UserDAO userDao;
+	private ActivityDAO activityDao;
+	private ReservationDAO reservationDao;
 
 	public UserDAO getUserDao() {
 		return userDao;
@@ -14,4 +18,20 @@ public class DaoFactoryImpl implements DaoFactory {
 	public void setUserDao(UserDAO userDao) {
 		this.userDao = userDao;
 	}
+
+	public ActivityDAO getActivityDao() {
+		return activityDao;
+	}
+
+	public void setActivityDao(ActivityDAO activityDao) {
+		this.activityDao = activityDao;
+	}
+
+	public ReservationDAO getReservationDao() {
+		return reservationDao;
+	}
+
+	public void setReservationDao(ReservationDAO reservationDao) {
+		this.reservationDao = reservationDao;
+	}	
 }
