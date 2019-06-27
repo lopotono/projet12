72867680fbcfada6a6882ccfb2a7b253e7a9ad82
lopotono <30410435+lopotono@users.noreply.github.com>
2019-06-27 16:10:46 +0200

    Add ReservationManager.java

---
+++ amopa-business/src/main/java/org/projet/amopa/business/contract/ReservationManager.java
@@ -0,0 +1,18 @@
+package org.projet.amopa.business.contract;
+
+import java.util.List;
+
+import org.projet.amopa.model.Reservation;
+
+public interface ReservationManager {
+	
+	List<Reservation> getListReservationByUserAndActivity(int id_user, int id_activity);
+	
+	void insertReservation(Reservation reservation);
+	
+	List<Reservation> getReservationByIdActivity(int id);
+	
+	List<Reservation> getReservationByUser(int id);
+	
+	List<Reservation> getReservations();
+}
