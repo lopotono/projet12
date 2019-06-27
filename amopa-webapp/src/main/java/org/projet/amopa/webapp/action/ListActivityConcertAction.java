f13ed56db1c67f5b4fff901ed33618d49849bd54
lopotono <30410435+lopotono@users.noreply.github.com>
2019-06-27 16:33:03 +0200

    Add ListActivityConcertAction.java

---
+++ amopa-webapp/src/main/java/org/projet/amopa/webapp/action/ListActivityConcertAction.java
@@ -0,0 +1,41 @@
+package org.projet.amopa.webapp.action;
+
+import java.util.List;
+
+import org.projet.amopa.model.Activity;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ListActivityConcertAction extends AbstractAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7430445039145016065L;
+	
+	private String genre;
+	private List<Activity> listActivityConcert;
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	public List<Activity> getListActivityConcert() {
+		return listActivityConcert;
+	}
+
+	public void setListActivityConcert(List<Activity> listActivityConcert) {
+		this.listActivityConcert = listActivityConcert;
+	}
+
+	public String execute() {
+		
+		listActivityConcert = getManagerFactory().getActivityManager().getListActivityByConcert(genre);
+		return ActionSupport.SUCCESS;
+		
+	}
+}
