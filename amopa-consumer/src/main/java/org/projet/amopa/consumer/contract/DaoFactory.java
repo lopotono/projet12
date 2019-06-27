628b789af5edf05ad881298fe98b138e414d1251
lopotono <30410435+lopotono@users.noreply.github.com>
2019-06-27 16:14:50 +0200

    Update DaoFactory.java

--- amopa-consumer/src/main/java/org/projet/amopa/consumer/contract/DaoFactory.java
+++ amopa-consumer/src/main/java/org/projet/amopa/consumer/contract/DaoFactory.java
@@ -3,5 +3,6 @@ package org.projet.amopa.consumer.contract;
 public interface DaoFactory {
 	
 	public UserDAO getUserDao();
-
+	public ActivityDAO getActivityDao();
+	public ReservationDAO getReservationDao();
 }
