e3386b2df6811ac2079bd6ee019f3f11922cd79e
lopotono <30410435+lopotono@users.noreply.github.com>
2019-06-27 16:10:05 +0200

    Update ManagerFactory.java

--- amopa-business/src/main/java/org/projet/amopa/business/contract/ManagerFactory.java
+++ amopa-business/src/main/java/org/projet/amopa/business/contract/ManagerFactory.java
@@ -3,5 +3,6 @@ package org.projet.amopa.business.contract;
 public interface ManagerFactory {
 	
 	UserManager getUserManager();
-
+	ActivityManager getActivityManager();
+	ReservationManager getReservationManager();
 }
