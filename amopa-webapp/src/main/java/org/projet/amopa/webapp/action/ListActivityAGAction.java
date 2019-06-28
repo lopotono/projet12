a3a7e043b3f61e2c079d6d859f04524a7427760b
lopotono <30410435+lopotono@users.noreply.github.com>
2019-06-28 18:42:07 +0200

    Add ListActivityAGAction.java

---
+++ amopa-webapp/src/main/java/org/projet/amopa/webapp/action/ListActivityAGAction.java
@@ -0,0 +1,32 @@
+package org.projet.amopa.webapp.action;
+
+import java.util.List;
+
+import org.projet.amopa.model.Activity;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ListActivityAGAction extends AbstractAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8900447775473764803L;
+	
+	private List<Activity> listActivityAG;
+	private String genre;
+	
+	public List<Activity> getListActivityAG() {
+		return listActivityAG;
+	}
+
+	public void setListActivityAG(List<Activity> listActivityAG) {
+		this.listActivityAG = listActivityAG;
+	}
+
+	public String execute() {
+		
+		listActivityAG = getManagerFactory().getActivityManager().getListActivityByAG(genre);		
+		return ActionSupport.SUCCESS;		
+	}
+}
