3e1efb677f4aee0cc0b9527629ac7712a2be8b9d
lopotono <30410435+lopotono@users.noreply.github.com>
2019-06-28 18:40:04 +0200

    Add ListActivityConfAction.java

---
+++ amopa-webapp/src/main/java/org/projet/amopa/webapp/action/ListActivityConfAction.java
@@ -0,0 +1,40 @@
+package org.projet.amopa.webapp.action;
+
+import java.util.List;
+
+import org.projet.amopa.model.Activity;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ListActivityConfAction extends AbstractAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8698196729996471882L;
+	
+	private List<Activity> listActivityConf;
+	private String genre;
+	
+	public List<Activity> getListActivityConf() {
+		return listActivityConf;
+	}
+
+	public void setListActivityConf(List<Activity> listActivityConf) {
+		this.listActivityConf = listActivityConf;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	public String execute() {
+		
+		listActivityConf = getManagerFactory().getActivityManager().getListActivityByConf(genre);
+		return ActionSupport.SUCCESS;
+	}	
+}
