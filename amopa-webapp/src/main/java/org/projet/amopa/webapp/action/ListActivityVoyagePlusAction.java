4d1fb2a24190f6ed57266904ac1dcf2af9a0bb6c
lopotono <30410435+lopotono@users.noreply.github.com>
2019-06-28 18:41:25 +0200

    Add ListActivityVoyagePlusAction.java

---
+++ amopa-webapp/src/main/java/org/projet/amopa/webapp/action/ListActivityVoyagePlusAction.java
@@ -0,0 +1,40 @@
+package org.projet.amopa.webapp.action;
+
+import java.util.List;
+
+import org.projet.amopa.model.Activity;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ListActivityVoyagePlusAction extends AbstractAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4467748236214062477L;
+	
+	private List<Activity> listActivityVoyagePlus;
+	private String genre;
+	
+	public List<Activity> getListActivityVoyagePlus() {
+		return listActivityVoyagePlus;
+	}
+
+	public void setListActivityVoyagePlus(List<Activity> listActivityVoyagePlus) {
+		this.listActivityVoyagePlus = listActivityVoyagePlus;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String execute() {
+		
+		listActivityVoyagePlus = getManagerFactory().getActivityManager().getListActivityByVoyageplus(genre);
+		return ActionSupport.SUCCESS;		
+	}
+}
