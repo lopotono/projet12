e5a6c046a3a14db7d758959b7d4900686ebd7524
lopotono <30410435+lopotono@users.noreply.github.com>
2019-07-08 15:59:57 +0200

    Update ReservationRowMapper.java

--- amopa-consumer/src/main/java/org/projet/amopa/consumer/impl/rowmapper/ReservationRowMapper.java
+++ amopa-consumer/src/main/java/org/projet/amopa/consumer/impl/rowmapper/ReservationRowMapper.java
@@ -17,15 +17,21 @@ public class ReservationRowMapper implements RowMapper<Reservation> {
 		vReservation.setDatereservation(cal);
 		vReservation.setNbreparticipants(pRS.getInt("participant"));
 		vReservation.setEtat(pRS.getString("etat"));
-		Calendar cal1 = Calendar.getInstance();
-		cal1.setTime(pRS.getDate("date_reservation_sms"));
-		vReservation.setDateresasms(cal1);
-		Calendar cal2 = Calendar.getInstance();
-		cal2.setTime(pRS.getDate("date_confirmation_sms"));
-		vReservation.setDateconfsms(cal2);
-		Calendar cal3 = Calendar.getInstance();
-		cal3.setTime(pRS.getDate("date_rappel_sms"));
-		vReservation.setDaterappelsms(cal3);
+		if (pRS.getDate("date_reservation_sms") != null) {
+			Calendar cal1 = Calendar.getInstance();
+			cal1.setTime(pRS.getDate("date_reservation_sms"));
+			vReservation.setDateresasms(cal1);
+		}
+		if (pRS.getDate("date_confirmation_sms") != null) {
+			Calendar cal2 = Calendar.getInstance();
+			cal2.setTime(pRS.getDate("date_confirmation_sms"));
+			vReservation.setDateconfsms(cal2);
+		}
+		if (pRS.getDate("date_rappel_sms") != null) {
+			Calendar cal3 = Calendar.getInstance();
+			cal3.setTime(pRS.getDate("date_rappel_sms"));
+			vReservation.setDaterappelsms(cal3);
+		}
 		vReservation.setId_activity(pRS.getInt("id_activity"));
 		vReservation.setId_user(pRS.getInt("id_utilisateur"));
 		return vReservation;
