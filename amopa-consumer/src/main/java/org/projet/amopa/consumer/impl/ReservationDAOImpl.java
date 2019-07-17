a64dee542a31848e7354c892d11baf97d8b75bcf
lopotono <30410435+lopotono@users.noreply.github.com>
2019-07-17 13:13:02 +0200

    Update ReservationDAOImpl.java

--- amopa-consumer/src/main/java/org/projet/amopa/consumer/impl/ReservationDAOImpl.java
+++ amopa-consumer/src/main/java/org/projet/amopa/consumer/impl/ReservationDAOImpl.java
@@ -14,7 +14,8 @@ public class ReservationDAOImpl extends AbstractDaoImpl implements ReservationDA
 
 	public List<Reservation> getListReservationByUserAndActivity(int id_user, int id_activity) {
 
-		String vSQL = "SELECT * FROM reservation WHERE id_activity AND id_user=" + id_activity + id_user;
+		String vSQL = "SELECT * FROM reservation WHERE id_activity='" + id_activity + "' AND id_utilisateur='" + id_user
+				+ "'";
 
 		ReservationRowMapper vRowMapper = new ReservationRowMapper();
 
@@ -49,60 +50,118 @@ public class ReservationDAOImpl extends AbstractDaoImpl implements ReservationDA
 	public List<Reservation> getReservationByIdActivity(int id) {
 
 		String vSQL = "SELECT * FROM reservation WHERE id_activity=" + id;
-		
+
 		ReservationRowMapper vRowMapper = new ReservationRowMapper();
-		
+
 		List<Reservation> reservation = getJdbcTemplate().query(vSQL, vRowMapper);
-		
+
 		return reservation;
 	}
 
 	public List<Reservation> getReservationByUser(int id) {
 
 		String vSQL = "SELECT * FROM reservation WHERE id_utilisateur=" + id;
-		
+
 		ReservationRowMapper vRowMapper = new ReservationRowMapper();
-		
+
 		List<Reservation> reservation = getJdbcTemplate().query(vSQL, vRowMapper);
-		
+
 		return reservation;
 	}
 
 	public List<Reservation> getReservations() {
 
 		String vSQL = "SELECT * FROM reservation ORDER BY date_reservation";
-		
+
 		ReservationRowMapper vRowMapper = new ReservationRowMapper();
-		
+
 		List<Reservation> reservation = getJdbcTemplate().query(vSQL, vRowMapper);
-		
+
 		return reservation;
 	}
 
 	public void deleteReservation(Reservation reservation) {
 
 		String vSQL = "DELETE FROM reservation WHERE id_reservation=:id_reservation";
-		
+
 		MapSqlParameterSource vParams = new MapSqlParameterSource();
 		vParams.addValue("id_reservation", reservation.getId(), Types.INTEGER);
+
+		NamedParameterJdbcTemplate vJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
+
+		try {
+			vJdbcTemplate.update(vSQL, vParams);
+		} catch (DuplicateKeyException vEx) {
+
+		}
+	}
+
+	public Reservation getReservation(int id) {
+
+		String vSQL = "SELECT * FROM reservation WHERE id_reservation=" + id;
+
+		ReservationRowMapper vRowMapper = new ReservationRowMapper();
+
+		List<Reservation> reservation = getJdbcTemplate().query(vSQL, vRowMapper);
+
+		return reservation.get(0);
+	}
+
+	public void canceledReservation(Reservation reservation) {
+
+		String vSQL = "DELETE FROM reservation WHERE id_reservation=:id_reservation";
+
+		MapSqlParameterSource vParams = new MapSqlParameterSource();
+		vParams.addValue("id_reservation", reservation.getId(), Types.INTEGER);
+
+		NamedParameterJdbcTemplate vJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
+
+		try {
+			vJdbcTemplate.update(vSQL, vParams);
+		} catch (DuplicateKeyException vEx) {
+
+		}
+	}
+
+	public void updateReservation(Reservation reservation) {
+
+		String vSQL = "UPDATE reservation SET date_reservation= :date_reservation, participant= :participant, etat= :etat, date_reservation_sms= :date_reservation_sms, date_confirmation_sms= :date_confirmation_sms, date_rappel_sms= :date_rappel_sms, id_activity= :id_activity, id_utilisateur= :id_utilisateur WHERE id_reservation= :id";
+
+		MapSqlParameterSource vParams = new MapSqlParameterSource();
+		vParams.addValue("id", reservation.getId(), Types.INTEGER);
+		vParams.addValue("date_reservation", reservation.getDatereservation(), Types.DATE);
+		vParams.addValue("participant", reservation.getNbreparticipants(), Types.INTEGER);
+		vParams.addValue("etat", reservation.getEtat(), Types.VARCHAR);
+		vParams.addValue("date_reservation_sms", reservation.getDateresasms(), Types.DATE);
+		vParams.addValue("date_confirmation_sms", reservation.getDateconfsms(), Types.DATE);
+		vParams.addValue("date_rappel_sms", reservation.getDaterappelsms(), Types.DATE);
+		vParams.addValue("id_activity", reservation.getId_activity(), Types.INTEGER);
+		vParams.addValue("id_utilisateur", reservation.getId_user(), Types.INTEGER);
+
+		NamedParameterJdbcTemplate vJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
+
+		try {
+			vJdbcTemplate.update(vSQL, vParams);
+		} catch (DuplicateKeyException vEx) {
+
+		}
+	}
+
+	public void confirmerReservation(Reservation reservation) {
+		
+		String vSQL = "UPDATE reservation SET etat= :etat, date_confirmation_sms= :date_confirmation_sms WHERE id_reservation= :id";
+		
+		MapSqlParameterSource vParams = new MapSqlParameterSource();
+		vParams.addValue("id", reservation.getId(), Types.INTEGER);
+		vParams.addValue("etat", reservation.getEtat(), Types.VARCHAR);
+		vParams.addValue("date_confirmation_sms", reservation.getDateconfsms(), Types.DATE);
 		
 		NamedParameterJdbcTemplate vJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
-		
+
 		try {
 			vJdbcTemplate.update(vSQL, vParams);
 		} catch (DuplicateKeyException vEx) {
 
 		}		
-	}
-
-	public Reservation getReservation(int id) {
-		
-		String vSQL = "SELECT * FROM reservation WHERE id_reservation=" + id;
-		
-		ReservationRowMapper vRowMapper = new ReservationRowMapper();
-		
-		List<Reservation> reservation = getJdbcTemplate().query(vSQL, vRowMapper);
-		
-		return reservation.get(0);
 	}
 }
